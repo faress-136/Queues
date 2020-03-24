9268fd7b3e661feeaa6d58dfa6540be69eaf395c
fares ramadan <fofas2000@yahoo.com>
2020-03-24 04:08:04 +0200

    Initial Commit

---
+++ templeteQueues/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  templeteQueues
+//
+//  Created by Fares Mohamed on 3/24/20.
+//  Copyright © 2020 Fares Mohamed. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
